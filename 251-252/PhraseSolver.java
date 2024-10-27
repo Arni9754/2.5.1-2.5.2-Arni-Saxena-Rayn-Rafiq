9fa6f8dcc7d13525b5d1690fdd34cd6010fc28db
Arni <153138713+Arni9754@users.noreply.github.com>
2024-10-26 22:59:36 -0700

    Update PhraseSolver.java

--- 251-252/PhraseSolver.java
+++ 251-252/PhraseSolver.java
@@ -8,9 +8,23 @@ import java.util.Scanner;
 public class PhraseSolver
 {
   /* your code here - attributes */
+  private String phrase = ""; //Step 4 Add Code
+  private int randomPointsPerCharacter = 0; //Step 4 Add Code
+
+  //Player player1 = new Player(); //Step 4 Add Code, //Step 16 Modify Code
+  //Player player2 = new Player(); //Step 4 Add Code, //Step 16 Modify Code
+  
+  boolean isSolved = false; //Modified Code
 
   /* your code here - constructor(s) */ 
+  public PhraseSolver() { //Step 7 Add Code
+    Player player1 = new Player(); //Step 8 Add Code, //Step 16 Modify Code
+    Player player2 = new Player(); //Step 8 Add Code, //Step 16 Modify Code
+    Board board = new Board(); //Step 8 Add Code, //Step 16 Modify Code
+    isSolved = false;   //Step 8 Add Code, //Step 16 Modify Code
 
+    
+  }
   /* your code here - accessor(s) */
   
   /* your code here - mutator(s)  */
